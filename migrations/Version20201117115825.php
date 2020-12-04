c6732f5a313fe97613486e40ee47033d2407947a
Eric <e.gigondan@gmail.com>
2020-12-04 19:51:08 +0100

    Add files via upload

---
+++ migrations/Version20201117115825.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201117115825 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE lieu (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, photo LONGTEXT NOT NULL, type VARCHAR(255) NOT NULL, prix LONGTEXT NOT NULL, horaires LONGTEXT NOT NULL, adresse LONGTEXT NOT NULL, transport LONGTEXT NOT NULL, evenement LONGTEXT NOT NULL, site_internet LONGTEXT NOT NULL, lien_maps LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE lieu');
+    }
+}
